48a6604412ac62168664cd25541beb929da96110
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-02-10 18:20:25 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <h2 className="text-green-600 text-xl mt-2">Full Stack Developer</h2>
       </section>
 
-      <section className="about w-full h-[800px] flex justify-center items-center">
+      <section className="about w-full h-[100dvh] flex justify-center items-center ">
         <div className="w-[90vw] flex flex-col-reverse justify-center items-center gap-6 md:flex-row md:w-[70vw]">
           <div className="w-[90%]">
             <h2 className="text-green-600 text-xl font-bold md:text-2xl">
