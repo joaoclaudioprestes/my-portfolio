bb5a29f7a7290539f57ae0521a807ca224560480
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-03-03 19:22:48 -0300

    Update index.tsx

--- src/components/UI/button/index.tsx
+++ src/components/UI/button/index.tsx
@@ -7,8 +7,8 @@ type ButtonProps = {
 
 export const Button = ({content, important, link}: ButtonProps) => {
   return (
-    <button className={`w-fit py-3 px-5 bg-white border border-1 border-black rounded-[15px] text-md ${important && `bg-black text-white`}`} href={link} >
-      <p>{content}</p>
+    <button className={`w-fit py-3 px-5 bg-white border border-1 border-black rounded-[15px] text-md ${important && `bg-black text-white`}`}>
+      <a href={link} className="decoration-none">{content}</a>
     </button>
   )
 }
