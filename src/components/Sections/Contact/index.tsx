82b7c1915d0412e56281fedccc123b776c24e12d
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-03-10 16:05:58 -0300

    Update index.tsx

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -4,30 +4,35 @@ import { FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 
 export const Contact = () => {
   return (
-    <section className="mx-auto">
-      <div className="title-contact">
-        <h2 className="text-3xl font-semibold">Contato</h2>
-        <p className="text-lg">Entre em contato comigo.</p>
-      </div>
-      <div className="info-contact flex justify-center mt-8">
-        <button className="box-contact linkedin flex items-center justify-center bg-blue-500 text-white px-4 py-2 rounded-md mr-4">
-          <FaLinkedin className="icon mr-2" />
-          <Link href="https://www.linkedin.com/in/jo%C3%A3o-claudio-prestes/" target="_blank" className="link">
-            LinkedIn
-          </Link>
-        </button>
-        <button className="box-contact whatsapp flex items-center justify-center bg-green-500 text-white px-4 py-2 rounded-md mr-4">
-          <FaWhatsapp className="icon mr-2" />
-          <Link href="tel:15997838020" className="link">
-            (15) 99783-8020
-          </Link>
-        </button>
-        <button className="box-contact email flex items-center justify-center bg-red-500 text-white px-4 py-2 rounded-md">
-          <MdEmail className="icon mr-2" />
-          <Link href="mailto:joaoprestes17@outlook.com" className="link">
-            joaoprestes17@outlook.com
-          </Link>
-        </button>
+    <section className="w-full py-20 flex flex-col items-center gap-14 md:justify-center md:items-start md:p-20">
+      <div className="w-[90vw] flex flex-col gap-5 m-auto md:w-[70vw]">
+        <div className="flex flex-col items-center">
+          <h2 className="text-2xl text-black">Contato</h2>
+          <p className="text-base text-black">Entre em contato comigo.</p>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-4 mt-4">
+          <button className="w-[90%] h-10 flex justify-around items-center bg-white border border-black rounded-lg md:w-[50%]">
+            <FaLinkedin className="relative right-4" />
+            <Link href="https://www.linkedin.com/in/jo%C3%A3o-claudio-prestes/" target="_blank" className="link text-base">
+              LinkedIn
+            </Link>
+            <span></span>
+          </button>
+          <button className="w-[90%] h-10 flex justify-around items-center bg-white border border-black rounded-lg md:w-[50%]">
+            <FaWhatsapp className="relative right-2" />
+            <Link href="tel:15997838020" className="">
+              (15) 99783-8020
+            </Link>
+            <span></span>
+          </button>
+          <button className="w-[90%] h-10 flex justify-around items-center bg-white border border-black rounded-lg md:w-[50%]">
+            <MdEmail className="relative left-1" />
+            <Link href="mailto:joaoprestes17@outlook.com" className="link text-base">
+              joaoprestes17@outlook.com
+            </Link>
+            <span></span>
+          </button>
+        </div>
       </div>
     </section>
   );
