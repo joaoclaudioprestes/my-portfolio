3d5203c69a95b5d62f9ea6c1843af1665c0295b1
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-03-10 18:03:13 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { Header } from "@/components/Sections/Header";
-import React from "react";
 import { HeroSection } from "./../components/Sections/HeroSection/index";
 import { About } from "@/components/Sections/About";
 import { Skills } from "@/components/Sections/Skills";
@@ -8,6 +6,7 @@ import { Experience } from "@/components/Sections/Experience";
 import { Services } from "@/components/Sections/Services";
 import { Contact } from "@/components/Sections/Contact";
 import { Footer } from "@/components/Sections/Footer";
+import { Header } from "@/components/Sections/Header";
 
 export default function Home() {
   return (
